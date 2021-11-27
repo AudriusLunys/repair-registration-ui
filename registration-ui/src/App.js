0c9d6b00d59023af4da4de0992d6b15f3c0e0513
AudriusLunys <lunysaudrius@inbox.lt>
2021-11-27 14:18:05 +0200

    home page component added

--- registration-ui/src/App.js
+++ registration-ui/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListOrders from "./components/ListOrders";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CreateOrder from "./components/CreateOrder";
+import Home from "./components/Home";
 
 function App() {
     return (
@@ -11,7 +13,9 @@ function App() {
                 <Header/>
                 <div className="container">
                     <Routes>
-                        <Route path='/orders' element={<ListOrders/>}></Route>
+                        <Route path='/' exact={true} element={<Home/>}></Route>
+                        <Route path='/orders' exact={true} element={<ListOrders/>}></Route>
+                        <Route path='/register-device' exact={true} element={<CreateOrder/>}></Route>
                     </Routes>
                 </div>
                 <Footer/>
---
+++ registration-ui/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+              <h1> home </h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
